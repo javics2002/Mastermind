1c8fa51feaee6691c959192ef05822d1bf115601
javics2002 <49459590+javics2002@users.noreply.github.com>
2023-09-21 19:37:18 +0200

    Callback del boton

---
+++ P0/app/lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,10 @@
+package com.example.lib;
+import javax.swing.JFrame;
+import java.awt.Panel;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.event.MouseListener;
+
+public class MyClass {
+    JFrame canvas = new JFrame();
+}
